e51dd87ec398ec4b36c4e5399d276e089d3563c3
조규환 <17679@hanmail.net>
2017-02-02 17:15:16 +0900

    #13 Issue Repository 및 Controller 추가
    * #13 - issue 기능 생성 및 레포지터리 생성
    
    *  #12 User CRUD 기능 추가
     * user 생성 - /user/new (get)
     * user 로그인/로그아웃 - /user/login (post), /user/logout (get)
    
    ed, and an empty message aborts the commit.
    
    * #13 - issue 변경사항 추가
    
    * #13 - 이슈 생성 추가사항

---
+++ src/main/java/infinitefire/project/domain/IssueRepository.java
@@ -0,0 +1,7 @@
+package infinitefire.project.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueRepository extends JpaRepository<Issue, Long>{
+
+}
--- src/main/java/infinitefire/project/web/IssueController.java
+++ src/main/java/infinitefire/project/web/IssueController.java
@@ -1,24 +1,107 @@
 package infinitefire.project.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import infinitefire.project.domain.Issue;
+import infinitefire.project.domain.IssueRepository;
+import infinitefire.project.domain.UserRepository;
+import infinitefire.project.utils.HttpSessionUtils;
 
 @Controller
 public class IssueController {
+	@Autowired
+	IssueRepository issueRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	private static final Logger log = LoggerFactory.getLogger(IssueController.class);
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		log.debug("Access >> /");
+		
+		model.addAttribute("issueList", issueRepository.findAll());
 		return "index";
 	}
 
 	@GetMapping("/issue/new")
-	public String form() {
-		return "issue/form";
+	public String createIssueForm(HttpSession session) {
+		log.debug("Access >> /issue/new-Get");
+		
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			log.info("info >> is not LoginUser");
+			return "issue/new";
+		}else {
+			log.info("info >> is LoginUser");
+			return "redirect:/";
+		}
+	}
+	@PostMapping("/issue/new")
+	public String createIssue(HttpSession session, Issue newIssue) {
+		log.debug("Access >> /issue/new-Post");
+		
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			log.info("info >> is not LoginUser");
+			newIssue.setWriter(HttpSessionUtils.getUserFromSession(session));
+			newIssue.setLabel(0);
+			newIssue.setState("close");
+			issueRepository.save(new Issue(
+					newIssue.getSubjects(),
+					newIssue.getContents(),
+					HttpSessionUtils.getUserFromSession(session),
+					0, "close"));
+			return "redirect:/";
+		}else {
+			return "redirect:/";
+		}
+	}
+	
+	@GetMapping("issue/{id}/modify")
+	public String modifyIssueForm(HttpSession session, @PathVariable Long id, Model model) {
+		log.debug("Access >> /issue/modify");
+		
+		if(HttpSessionUtils.isLoginUser(session)) {
+			model.addAttribute("issue", issueRepository.findOne(id));
+			return "issue/modify";
+		}else
+			return "redirect:/";
+	}
+	@PutMapping("issue/{id}/modify")
+	public String modifiedIssue(HttpSession session, @PathVariable Long id, Issue modifIssue) {
+		log.debug("Access >> /issue/{" + id +"}/modify");
+		
+		if(HttpSessionUtils.isLoginUser(session)) {
+			issueRepository.save(modifIssue);
+			return "/";
+		}else
+			return "redirect:/";
+	}
+	
+	@DeleteMapping("issue/{id}/delete")
+	public String deleteIssue(HttpSession session, @PathVariable Long id) {
+		log.debug("Access >> /issue/{" + id + "}/delete");
+		
+		if(HttpSessionUtils.isLoginUser(session)) {
+			issueRepository.delete(id);
+			return "/";
+		}else
+			return "redirect:/";
 	}
 
-	@GetMapping("/issue/list")
-	public String show() {
-		return "issue/show";
+	@GetMapping("/issue/detail")
+	public String showIssueDetail() {
+		return "issue/detail";
 	}
-
 }
