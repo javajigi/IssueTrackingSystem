691bb05dbef5c38d7c0b57bad9470dc779c0eb03
choijaewoong <jwchoi0129@gmail.com>
2017-02-03 10:53:19 +0900

    import.sql 더미 데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,4 +4,11 @@ INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(3
 INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(4, 'Fourth Milestone', 'Fourth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(5, 'Fifth Milestone', 'Fifth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-insert into user (id, user_id, name, password, state) values (1, 'test', 'jkh', '123', 'y')
+insert into user (id, user_id, name, password, state) values (1, 'test', 'jkh', '1234', 'y');
+insert into user (id, user_id, name, password, state) values (2, 'test2', 'jaewoong', '1234', 'y');
+insert into user (id, user_id, name, password, state) values (3, 'test3', 'subin', '1234', 'y');
+
+INSERT INTO issue (id, contents, label, state, name, write_date, writer_id) VALUES (1, 'first issue contents', 0, 'close', 'first issue', CURRENT_TIMESTAMP(), 1);
+INSERT INTO issue (id, contents, label, state, name, write_date, writer_id) VALUES (2, 'second issue contents', 0, 'close', 'second issue', CURRENT_TIMESTAMP(), 2);
+INSERT INTO issue (id, contents, label, state, name, write_date, writer_id) VALUES (3, 'third issue contents', 0, 'close', 'third issue', CURRENT_TIMESTAMP(), 1);
+INSERT INTO issue (id, contents, label, state, name, write_date, writer_id) VALUES (4, 'fourth issue contents', 0, 'close', 'fourth issue', CURRENT_TIMESTAMP(), 3);
