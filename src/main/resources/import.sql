09a71bcf8651681d2d44cdf47fbda323187ef7fc
hihihot2 <17679@hanmail.net>
2017-02-03 09:52:38 +0900

    #17 - 로그인 로그아웃 표시 문제 해결 및 import.sql 소문자 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO MILESTONE (id, subject, description, start_date, end_date ) VALUES(1, 'First Milestone', 'First Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO MILESTONE (id, subject, description, start_date, end_date ) VALUES(2, 'Second Milestone', 'Second Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO MILESTONE (id, subject, description, start_date, end_date ) VALUES(3, 'Third Milestone', 'Third Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO MILESTONE (id, subject, description, start_date, end_date ) VALUES(4, 'Fourth Milestone', 'Fourth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO MILESTONE (id, subject, description, start_date, end_date ) VALUES(5, 'Fifth Milestone', 'Fifth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(1, 'First Milestone', 'First Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(2, 'Second Milestone', 'Second Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(3, 'Third Milestone', 'Third Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(4, 'Fourth Milestone', 'Fourth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO milestone (id, subject, description, start_date, end_date ) VALUES(5, 'Fifth Milestone', 'Fifth Milestone Description', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 insert into user (id, user_id, name, password, state) values (1, 'test', 'jkh', '123', 'y')
