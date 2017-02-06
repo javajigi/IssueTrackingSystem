23c6dfd6c050fa747e94cb910f6328767448b78b
hihihot2 <17679@hanmail.net>
2017-02-06 20:35:23 +0900

    $40 Comment 생성 작업 완료 - 비동기 처리

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -13,12 +13,25 @@ function addComment(e) {
 		url: url,
 		data: queryString,
 		success: function(result) {
-			console.log('success-RequestData\n',result);
+			console.log(result);
 			
-			
+			var template = $("#commentTemplate").html();
+			var returntemp = template.format(result.id, result.contents, result.formattedWriteDate, result.writer.id, result.writer.userId);
+			$(".comment-form").append(returntemp);
+			$("textarea[name=contents]").val("");
 		},
 		error: function(error) {
 			console.log('fail-RequestData');
 		}
 	});
 }
+
+String.prototype.format = function() {
+	  var args = arguments;
+	  return this.replace(/{(\d+)}/g, function(match, number) {
+	    return typeof args[number] != 'undefined'
+	        ? args[number]
+	        : match
+	        ;
+	  });
+	};
