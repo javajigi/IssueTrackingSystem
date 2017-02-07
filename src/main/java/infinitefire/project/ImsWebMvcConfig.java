a5c8b433449e80b648fba7782af480df135fa956
hihihot2 <17679@hanmail.net>
2017-02-07 09:36:08 +0900

    #21 jquery dependency 변경

--- src/main/java/infinitefire/project/ImsWebMvcConfig.java
+++ src/main/java/infinitefire/project/ImsWebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import infinitefire.project.security.LoginUserHandlerMethodArgumentResolver;
@@ -19,4 +20,9 @@ public class ImsWebMvcConfig extends WebMvcConfigurerAdapter {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
     }
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
