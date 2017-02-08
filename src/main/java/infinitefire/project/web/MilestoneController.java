668631c996a359e4ce0f81d637b1cb3f1ad3bb51
hihihot2 <17679@hanmail.net>
2017-02-08 16:31:17 +0900

    #14 milestone 작업물 수정 ->@loginUser 추가

--- src/main/java/infinitefire/project/web/MilestoneController.java
+++ src/main/java/infinitefire/project/web/MilestoneController.java
@@ -1,27 +1,21 @@
 package infinitefire.project.web;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import infinitefire.project.domain.Milestone;
 import infinitefire.project.domain.MilestoneRepository;
+import infinitefire.project.domain.User;
+import infinitefire.project.security.LoginUser;
 import infinitefire.project.utils.HttpSessionUtils;
 
 @Controller
@@ -34,35 +28,26 @@ public class MilestoneController {
 	private MilestoneRepository milestoneRepository;
 	
 	@GetMapping("/new")
-	public String createform(HttpSession session) {		
-		if(HttpSessionUtils.isLoginUser(session)) {
-			return "/milestone/new";
-		}else {
-			return "/user/login";
-		}
+	public String createform(@LoginUser User loginUser) {		
+		return "/milestone/new";
 	}
 	
 	// TODO @LoginUser를 사용해 통일성을 가져간다.
 	@PostMapping("/new")
-	public String create(Milestone milestone, HttpSession session){
-		
-		if(HttpSessionUtils.isLoginUser(session)) {
-			milestoneRepository.save(milestone);
-			return "redirect:/milestone/list";
-		} else {
-			return "redirect:/user/login";
-		}
+	public String create(@LoginUser User loginUser, Milestone milestone){
+		milestoneRepository.save(milestone);
+		return "redirect:/milestone/list";
 	}
 	
 	
 	@GetMapping("/list")
-	public String show(Model model) {			
+	public String show(@LoginUser User loginUser, Model model) {			
 		model.addAttribute("milestones", milestoneRepository.findAll());
 		return "milestone/list";
 	}
 	
 	@GetMapping("/{id}/detail")
-	public String detail(@PathVariable long id, Model model){
+	public String detail(@LoginUser User loginUser, @PathVariable long id, Model model){
 		
 		Milestone milestone = milestoneRepository.findOne(id);
 		model.addAttribute("milestone", milestone);
