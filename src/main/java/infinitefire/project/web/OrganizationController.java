585ce7f5da43fda6d62e3e5ff387e35aa9b16744
hihihot2 <17679@hanmail.net>
2017-02-14 16:17:22 +0900

    #94 OrganizationController 정의

---
+++ src/main/java/infinitefire/project/web/OrganizationController.java
@@ -0,0 +1,10 @@
+package infinitefire.project.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/group")
+public class OrganizationController {
+
+}
