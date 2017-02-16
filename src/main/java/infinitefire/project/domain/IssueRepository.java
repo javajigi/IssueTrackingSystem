9904eec3459c50853c57792000a96961d519ad81
hihihot2 <17679@hanmail.net>
2017-02-16 14:14:04 +0900

    #106 sorting 개선 작업

--- src/main/java/infinitefire/project/domain/IssueRepository.java
+++ src/main/java/infinitefire/project/domain/IssueRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface IssueRepository extends JpaRepository<Issue, Long>{
 	
 	List<Issue> findByState(IssueState state);
+	List<Issue> findByOrganizationIdAndState(Long groupId, IssueState state);
+	
 	//날짜순-최신
-	List<Issue> findByIdOrderByWriteDateAsc(Long id);
+	//List<Issue> findByIdOrderByWriteDateAsc(Long id);
+	List<Issue> findAllByOrderByWriteDateAsc();
 	//날짜순-오래된
-	List<Issue> findByIdOrderByWriteDateDesc(Long id);
+	List<Issue> findAllByOrderByWriteDateDesc();
 	//댓글순-많은
 	List<Issue> findByIdOrderByCountCommentAsc(Long id);
 	//댓글순-적은
