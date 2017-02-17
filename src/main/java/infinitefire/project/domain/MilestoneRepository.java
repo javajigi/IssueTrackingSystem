3833382bcd7bd286a97f96e6f7b5b3c66df6e7d2
hihihot2 <17679@hanmail.net>
2017-02-17 10:52:52 +0900

    #132 milestone list보기 -> organization설정으로 속한 그룹의 마일스톤만 보여지게 수정

--- src/main/java/infinitefire/project/domain/MilestoneRepository.java
+++ src/main/java/infinitefire/project/domain/MilestoneRepository.java
@@ -1,8 +1,9 @@
 package infinitefire.project.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface MilestoneRepository extends CrudRepository<Milestone, Long> {
-	
-
+	List<Milestone> findByOrganizationOrderByStartDateDesc(Organization organization);
 }
