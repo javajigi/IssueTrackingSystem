502c174570d8e0691f055ea5e69251071975aff4
javajigi <javajigi@gmail.com>
2017-03-03 17:38:21 +0900

    create new image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./mvnw clean package docker:build -DdockerImageTags=1.0.2
+APP_NAME=its
+
+COMMIT_HASH="$(git show-ref --head | grep -h HEAD | cut -d':' -f2 | head -n 1 | head -c 10)"
+
+./mvnw clean package docker:build -DdockerImageTags=its_$COMMIT_HASH
 
 docker-compose up --build -d
